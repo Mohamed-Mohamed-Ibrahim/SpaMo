8dd33b61dd96e21ba258b38622541fc8f220ff9c
Hossam-Osama <osamahossam899@gmail.com>
2025-11-21 17:10:21 +0200

    fast,small

--- dataset/datamodule.py
+++ dataset/datamodule.py
@@ -1,12 +1,13 @@
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from utils.helpers import instantiate_from_config
-
+import os
 
 class DataModuleFromConfig(pl.LightningDataModule):
+
     def __init__(self, batch_size, train=None, validation=None, test=None, num_workers=None):
         super().__init__()
-
+        
         self.batch_size = batch_size
         self.dataset_configs = dict()
         self.num_workers = num_workers if num_workers is not None else batch_size * 2
@@ -30,7 +31,9 @@ class DataModuleFromConfig(pl.LightningDataModule):
         return DataLoader(
             dataset=self.datasets['train'], 
             batch_size=self.batch_size,
-            num_workers=self.num_workers,
+            pin_memory=True,
+            num_workers=os.cpu_count(),
+            persistent_workers=True,
             shuffle=True,
             # collate_fn=BaseFeeder.collate_fn if self.use_collate else None
             collate_fn=self.datasets['train'].collate_fn
@@ -40,7 +43,9 @@ class DataModuleFromConfig(pl.LightningDataModule):
         return DataLoader(
             dataset=self.datasets['valid'], 
             batch_size=self.batch_size,
-            num_workers=self.num_workers,
+            pin_memory=True,
+            num_workers=os.cpu_count(),
+            persistent_workers=True,
             shuffle=False,
             # collate_fn=BaseFeeder.collate_fn if self.use_collate else None,
             collate_fn=self.datasets['valid'].collate_fn
@@ -50,7 +55,9 @@ class DataModuleFromConfig(pl.LightningDataModule):
         return DataLoader(
             dataset=self.datasets['test'], 
             batch_size=self.batch_size,
-            num_workers=self.num_workers,
+            pin_memory=True,
+            num_workers=os.cpu_count(),
+            persistent_workers=True,
             shuffle=False,
             # collate_fn=BaseFeeder.collate_fn if self.use_collate else None
             collate_fn=self.datasets['test'].collate_fn
