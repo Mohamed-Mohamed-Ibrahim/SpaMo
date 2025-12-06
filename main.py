e9357b2326b0da4921e7416df847c402a20ef88d
Omar H.Bishr <109454477+OmarHani4306@users.noreply.github.com>
2025-12-06 23:31:31 +0200

    Comment out testing on training data
    Comment out code for testing on training data.

--- main.py
+++ main.py
@@ -335,6 +335,10 @@ def main():
                 trainer.test(model, data)
     elif opt.test:
         trainer.validate(model, data, ckpt_path=ckpt) # trainer.test(model, data, ckpt_path=ckpt)
+        # if you want to test on train data add these and comment above
+        # print("!!! OVERRIDE: Testing on TRAIN data set (as requested) !!!")
+        # train_loader = data.train_dataloader()
+        # trainer.test(model, dataloaders=train_loader, ckpt_path=ckpt)
 
 
 if __name__ == '__main__':
