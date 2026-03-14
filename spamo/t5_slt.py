a8b068be43ec0255aba16e95693d4cf021181ed4
mohassan5286 <mohassan5286@gmail.com>
2026-03-14 04:26:45 +0200

    fix a boundary problem of vis_sep

--- spamo/t5_slt.py
+++ spamo/t5_slt.py
@@ -181,7 +181,7 @@ class FlanT5SLT(AbstractSLT):
         
         self.temporal_encoder = TemporalConv(self.inter_hidden, self.inter_hidden)
         
-        self.vis_sep = nn.Parameter(torch.randn(5, self.inter_hidden) * 0.02)
+        self.vis_sep = nn.Parameter(torch.randn(16, self.inter_hidden) * 0.02)
         
         if self.fusion_mode == 'adaptive':
             self.adaptive_fusion = AdaptiveFusion(
