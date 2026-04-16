99f4d558301fea3d0eb5a12e271a36f060bb02f6
mohassan5286 <mohassan5286@gmail.com>
2026-04-16 21:28:52 +0200

    update the required for slowfast

--- spamo/t5_slt.py
+++ spamo/t5_slt.py
@@ -174,7 +174,7 @@ class FlanT5SLT(AbstractSLT):
             max_length=self.max_txt_len,
         )
 
-        self.spatio_proj = build_vision_projector('linear', 2048, self.inter_hidden)
+        self.spatio_proj = build_vision_projector('linear', self.input_size, self.inter_hidden)
         self.spatiotemp_proj = build_vision_projector('linear', 1024, self.inter_hidden)
         self.pose_proj = build_vision_projector('linear', self.pose_input_size, self.inter_hidden)
         self.fusion_proj = build_vision_projector('mlp2x_gelu', self.inter_hidden, self.t5_model.config.hidden_size)
