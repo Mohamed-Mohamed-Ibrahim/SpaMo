f8d436311d9210365852d50735a4d42c11828531
Mohamed Mohamed Ibrahim <mohamedgawad600@gmail.com>
2026-04-17 17:26:05 +0200

    Adding start_time in How2Sign only.

--- scripts/vit_extract_feature.py
+++ scripts/vit_extract_feature.py
@@ -155,12 +155,13 @@ def get_iterator(args, mode):
         for i in range(num):
             fname = data[i]["folder"]
             file_id = data[i]["fileid"]
-            start_time = data[i].get("original_info", {}).get("START_REALIGNED", None)
+            start_time = None
 
             if ds_name in ["Phoenix14T", "CSL-Daily"]:
                 image_list = get_img_list(ds_name, args.video_root, fname)
             else:
                 if ds_name == "How2Sign":
+                    start_time = data[i].get("original_info", {}).get("START_REALIGNED", None)
                     image_list = read_video(
                         fname,
                         start_time=start_time,
