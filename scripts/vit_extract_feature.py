05385890fadca4b9eee62597a2957456cc5ff202
Mohamed Mohamed Ibrahim <mohamedgawad600@gmail.com>
2026-05-18 21:16:12 +0300

    Removing dev and test modes.

--- scripts/vit_extract_feature.py
+++ scripts/vit_extract_feature.py
@@ -222,7 +222,7 @@ def get_iterator(args, mode):
 
 
 def main():
-    mode = ["train", "dev", "test"]
+    mode = ["train"]
     for m in mode:
         parser = get_parser()
         args = parser.parse_args()
